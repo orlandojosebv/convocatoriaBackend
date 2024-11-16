efd3aafd9fb33c805cd6d763649e8484cbe74345
Orlando <orlandjosebv@ufps.edu.co>
2024-11-16 16:49:09 -0500

    configuracion del swagger para vercel

--- config/swagger.js
+++ config/swagger.js
@@ -18,8 +18,7 @@ const swaggerOptions = {
 const swaggerSpec = swaggerJSDoc(swaggerOptions);
 
 const swaggerDocs = (app, port) =>{
-    app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-    app.get("/api-docs.json",(req,res)=>{
+    app.use("/api-docs", cors({ origin: "*" }), swaggerUi.serve, swaggerUi.setup(swaggerSpec));    app.get("/api-docs.json",(req,res)=>{
         res.setHeader("Content-Type","application/json");
         res.send(swaggerSpec);
     });

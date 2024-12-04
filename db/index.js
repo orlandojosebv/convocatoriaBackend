702112616f1888ebc0f8d30d0b2448d3cf163917
Jhoan Pérez <jhoansebastianperez73@gmail.com>
2024-12-04 15:00:28 -0500

    Fix: increment time waiting for connect to DB

--- db/index.js
+++ db/index.js
@@ -1,13 +1,22 @@
 const { Sequelize, DataTypes, Model } = require("sequelize");
 const mysql2 = require("mysql2");
-const { DB_NAME, DB_HOST, DB_PASSWORD, DB_USER , DB_PORT, TABLE_NAMES} = require("../config");
+const { DB_NAME, DB_HOST, DB_PASSWORD, DB_USER, DB_PORT, TABLE_NAMES } = require("../config");
 
 const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
   dialect: "mysql",
   dialectModule: mysql2,
   host: DB_HOST,
   port: DB_PORT,
-  logging: false
+  logging: false,
+  dialectOptions: {
+    connectTimeout: 60000, // Incrementar tiempo de espera a 60 segundos
+  },
+  pool: {
+    max: 5, // Máximo número de conexiones
+    min: 0, // Mínimo número de conexiones
+    acquire: 60000, // Tiempo de espera al adquirir conexión
+    idle: 10000, // Tiempo de inactividad antes de cerrar la conexión
+  },
 });
 
 sequelize.authenticate()
